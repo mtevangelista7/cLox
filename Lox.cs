68eeb4d25714164d74e28cd9fad3769243f7ae42
Matheus Evangelista Dos Santos <matheus.evangelista7@protonmail.com>
2024-05-08 16:14:55 -0300

    feat: add project

---
+++ Lox.cs
@@ -0,0 +1,50 @@
+﻿namespace Lox
+{
+    internal class Lox
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: clox [script]");
+                return;
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string pathName)
+        {
+            byte[] bytes = File.ReadAllBytes(Path.GetFullPath(pathName));
+            // run bytes
+        }
+
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                Console.WriteLine("> ");
+
+                var line = Console.ReadLine();
+
+                if (line is null) { break; }
+
+                Run(line);
+            }
+        }
+
+        private static void Run(string source)
+        {
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.scanTokens();
+
+            tokens.ForEach(t => Console.WriteLine(t.ToString()));
+        }
+    }
+}
